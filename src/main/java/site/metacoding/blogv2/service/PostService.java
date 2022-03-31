781da18f08936a9b37d887a9c3bac2b715c1eccf
ssar <ssar@nate.com>
2022-03-31 14:32:05 +0900

    16. 글상세보기 완료

--- src/main/java/site/metacoding/blogv2/service/PostService.java
+++ src/main/java/site/metacoding/blogv2/service/PostService.java
@@ -1,5 +1,7 @@
 package site.metacoding.blogv2.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
@@ -17,6 +19,16 @@ import site.metacoding.blogv2.domain.post.PostRepository;
 @Service // 컴포넌트 스캔시에 IoC 컨테이너에 등록됨 // 트랜잭션 관리하는 오브젝트임. 기능 모임
 public class PostService {
     private final PostRepository postRepository;
+
+    public Post 글상세보기(Integer id) {
+        Optional<Post> postOp = postRepository.findById(id);
+
+        if (postOp.isPresent()) {
+            return postOp.get();
+        } else {
+            throw new RuntimeException("해당 게시글을 찾을 수 없습니다");
+        }
+    }
 
     public Page<Post> 게시글목록(Integer page) {
         PageRequest pq = PageRequest.of(page, 3, Sort.by(Direction.DESC, "id"));
--- src/main/java/site/metacoding/blogv2/web/PostController.java
+++ src/main/java/site/metacoding/blogv2/web/PostController.java
@@ -1,16 +1,25 @@
 package site.metacoding.blogv2.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import lombok.RequiredArgsConstructor;
+import site.metacoding.blogv2.domain.post.Post;
 import site.metacoding.blogv2.service.PostService;
 
 @RequiredArgsConstructor
 @Controller
 public class PostController {
     private final PostService postService;
+
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable Integer id, Model model) {
+        Post postEntity = postService.글상세보기(id);
+        model.addAttribute("post", postEntity);
+        return "post/detail";
+    }
 
     // 페이지를 줘
     // /s 붙었으니까 자동으로 인터셉터가 인증 체크함. (완)
