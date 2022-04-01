4f95b43549a0b527710240427b8bb50430cc31ca
ssar <ssar@nate.com>
2022-04-01 12:00:39 +0900

    17. 글 삭제하기 완료

--- src/main/java/site/metacoding/blogv2/service/PostService.java
+++ src/main/java/site/metacoding/blogv2/service/PostService.java
@@ -20,6 +20,11 @@ import site.metacoding.blogv2.domain.post.PostRepository;
 public class PostService {
     private final PostRepository postRepository;
 
+    @Transactional
+    public void 글삭제하기(Integer id) {
+        postRepository.deleteById(id);
+    }
+
     public Post 글상세보기(Integer id) {
         Optional<Post> postOp = postRepository.findById(id);
 
--- src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
+++ src/main/java/site/metacoding/blogv2/web/api/PostApiController.java
@@ -1,12 +1,9 @@
 package site.metacoding.blogv2.web.api;
 
-import java.util.List;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +23,13 @@ import site.metacoding.blogv2.web.api.dto.post.WriteDto;
 public class PostApiController {
     private final PostService postService;
     private final HttpSession session;
+
+    @DeleteMapping("/s/api/post/{id}")
+    public ResponseDto<?> deleteById(@PathVariable Integer id) {
+        postService.글삭제하기(id);
+
+        return new ResponseDto<>(1, "성공", null);
+    }
 
     @GetMapping("/api/post/{id}")
     public ResponseDto<?> detail(@PathVariable Integer id) {
